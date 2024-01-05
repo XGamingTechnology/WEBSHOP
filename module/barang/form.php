d8bf129e8ddc47326483b1033bad1fd6974958c2
Kiky <rizkyramdani1402@gmai.com>
2024-01-05 16:51:24 +0700

    menambahkan cekeditor pada form barang

--- module/barang/form.php
+++ module/barang/form.php
@@ -1,38 +1,41 @@
-<?php 
+<?php
 
-    $barang_id = isset($_GET['barang_id']) ? $_GET['barang_id'] : false;
-
-    $nama_barang = "";
-    $kategori_id = "";
-    $spesifikasi = "";
-    $gambar = "";
-    $stok = "";
-    $harga = "";
-    $status = "";
-    $keterangan_gambar = "";
-    $button = "Add";
-
-    if ($barang_id) {
-        $query = mysqli_query ($koneksi, "SELECT * FROM barang WHERE barang_id='$barang_id'");
-        $row = mysqli_fetch_assoc($query);
-
-        $nama_barang = $row['nama_barang'];
-        $kategori_id = $row['kategori_id'];
-        $spesifikasi = $row['spesifikasi'];
-        $gambar = $row['gambar'];
-        $harga = $row['harga'];
-        $stok = $row['stok'];
-        $status = $row['status'];
-        $button = "update";
-
-        $keterangan_gambar = "(Klik choice file jika ingin menganti gambar disamping)";
-        $gambar = "<img src='".BASE_URL."images/barang/$gambar' style='width: 200px;vertical-align: middle;'/>";
-        
-    }
+	$barang_id = isset($_GET['barang_id']) ? $_GET['barang_id'] : false;
+	
+	$nama_barang = "";
+	$kategori_id = "";
+	$spesifikasi = "";
+	$gambar = "";
+	$stok = "";
+	$harga = "";
+	$status = "";
+	$keterangan_gambar = "";
+	$button = "Add";
+	
+	if($barang_id){
+		$query = mysqli_query($koneksi, "SELECT * FROM barang WHERE barang_id='$barang_id'");
+		$row = mysqli_fetch_assoc($query);
+		
+		$nama_barang = $row['nama_barang'];
+		$kategori_id = $row['kategori_id'];
+		$spesifikasi = $row['spesifikasi'];
+		$gambar = $row['gambar'];
+		$harga = $row['harga'];
+		$stok = $row['stok'];
+		$status = $row['status'];
+		$button = "Update";
+		
+		$keterangan_gambar = "(Klik pilih gambar jika ingin mengganti gambar disamping)";
+		$gambar = "<img src='".BASE_URL."images/barang/$gambar' style='width: 200px;vertical-align: middle;' />";
+	}
 
 ?>
 
-<form action="<?php echo BASE_URL."module/barang/action.php?barang_id=$barang_id";?>" method="POST"
+<!-- <script src="<?php echo BASE_URL."js/ckeditor/ckeditor.js"; ?>"></script> -->
+
+<!-- <script src="//cdn.ckeditor.com/4.23.0-lts/basic/ckeditor.js"></script> -->
+
+<form action="<?php echo BASE_URL."module/barang/action.php?barang_id=$barang_id"; ?>" method="POST"
     enctype="multipart/form-data">
 
     <div class="element-form">
@@ -40,17 +43,18 @@
         <span>
 
             <select name="kategori_id">
-                <?php 
-                    $query = mysqli_query($koneksi, "SELECT kategori_id, kategori FROM kategori WHERE status='on' ORDER BY kategori ASC");
-                    while($row=mysqli_fetch_assoc($query)){
-                        if($kategori_id == $row['kategori_id']) {
-                            echo "<option value='$row[kategori_id]' selected='true'>$row[kategori]</option>";    
-                        } else {
-                            echo "<option value='$row[kategori_id]'>$row[kategori]</option>";
-                        }
-                    }
-                ?>
+                <?php
+					$query = mysqli_query($koneksi, "SELECT kategori_id, kategori FROM kategori WHERE status='on' ORDER BY kategori ASC");
+					while($row=mysqli_fetch_assoc($query)){
+						if($kategori_id == $row['kategori_id']){
+							echo "<option value='$row[kategori_id]' selected='true'>$row[kategori]</option>";
+						}else{
+							echo "<option value='$row[kategori_id]'>$row[kategori]</option>";
+						}
+					}
+				?>
             </select>
+
         </span>
     </div>
 
@@ -59,11 +63,9 @@
         <span><input type="text" name="nama_barang" value="<?php echo $nama_barang; ?>" /></span>
     </div>
 
-    <div class="element-form">
-        <label>Spesifikasi</label>
-        <span>
-            <textarea name="spesifikasi"><?php echo $spesifikasi; ?></textarea>
-        </span>
+    <div style="margin-bottom:10px">
+        <label style="font-weight:bold">Spesifikasi</label>
+        <span><textarea name="spesifikasi" id="editor"><?php echo $spesifikasi; ?></textarea></span>
     </div>
 
     <div class="element-form">
@@ -79,15 +81,15 @@
     <div class="element-form">
         <label>Gambar Produk <?php echo $keterangan_gambar; ?></label>
         <span>
-            <input type="file" name="file" /> <?php echo $gambar?>
+            <input type="file" name="file" /> <?php echo $gambar; ?>
         </span>
     </div>
 
     <div class="element-form">
         <label>Status</label>
         <span>
-            <input type="radio" name="status" value="on" <?php if($status == "on") {echo "checked='true'";} ?> /> On
-            <input type="radio" name="status" value="off" <?php if($status == "off") {echo "checked='true'";} ?> /> off
+            <input type="radio" name="status" value="on" <?php if($status == "on"){ echo "checked='true'"; } ?> /> On
+            <input type="radio" name="status" value="off" <?php if($status == "off"){ echo "checked='true'"; } ?> /> Off
         </span>
     </div>
 
@@ -95,5 +97,13 @@
         <span><input type="submit" name="button" value="<?php echo $button; ?>" /></span>
     </div>
 
-
 </form>
+<script src="https://cdn.ckeditor.com/ckeditor5/40.2.0/classic/ckeditor.js"></script>
+
+<script>
+ClassicEditor
+    .create(document.querySelector('#editor'))
+    .catch(error => {
+        console.error(error);
+    });
+</script>
