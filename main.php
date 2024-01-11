62410ddc56a31ded04730c01f3367bce8bb6a4cb
Kiky <rizkyramdani1402@gmai.com>
2024-01-11 22:50:34 +0700

    menambahkan kategori barang pada main.php

--- main.php
+++ main.php
@@ -22,10 +22,22 @@
     <div id="frame-barang">
         <ul>
             <?php 
-            $query = mysqli_query($koneksi, "SELECT * FROM barang WHERE status='on'");
+            if($kategori_id){
+                $query = mysqli_query($koneksi, "SELECT * FROM barang WHERE status='on' AND kategori_id='$kategori_id' ORDER BY rand() DESC LIMIT 9");    
+            }else {
+                $query = mysqli_query($koneksi, "SELECT * FROM barang WHERE status='on' ORDER BY rand() DESC LIMIT 9");
 
+            }
+
+            $no=1;
             while($row=mysqli_fetch_assoc($query)) {
-                echo "<li>
+                
+                $style=false;
+                if($no == 3) {
+                    $style="style='margin-right:0px'";
+                    $no=0;
+                }
+                echo "<li $style>
                         <p class='price'>".rupiah($row['harga']).
                         "</P>
                         <a href='".BASE_URL."index.php?page=detail&barang_id=$row[barang_id]'>
@@ -38,6 +50,7 @@
                         <div class='button-add-cart'>
                              <a href='".BASE_URL."tambah_keranjang.php?barang_id=$row[barang_id]'>+ add to cart</a>
                         </div>";
+                $no++;
             }
                     
             ?>
